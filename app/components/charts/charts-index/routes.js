85aedd9cea056411cda53e2e3d20c9b200006326
Maxime HEBRARD <maxime.hebrard@gmail.com>
2017-01-05 17:52:31 +0900

    /charts works (one category only)

--- app/components/charts/charts-index/routes.js
+++ app/components/charts/charts-index/routes.js
@@ -1,10 +1,15 @@
 import angular from 'angular';
 
+import chartsComponent from './component';
 import clonalComponent from '../clonal/component';
 
 routeConfig.$inject = ['$routeProvider'];
 function routeConfig($routeProvider) {
   $routeProvider
+    .when('/charts', {
+      template: '<charts data-package="$resolve.dataPackage"></charts>',
+      datapackageUrl: 'components/charts/charts-index/datapackage.json'
+    })
     .when('/charts/clonal', {
       template: '<clonal data-package="$resolve.dataPackage"></clonal>',
       datapackageUrl: 'components/charts/clonal/datapackage.json'
@@ -14,5 +19,6 @@ function routeConfig($routeProvider) {
 
 export default angular
   .module('charts-index', ['projectX.dataService'])
+  .component('charts-idx', chartsComponent)
   .component('clonal', clonalComponent)
   .config(routeConfig);
