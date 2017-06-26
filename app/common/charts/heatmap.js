a9b8582ea408d3becbd689b8b9d33ed613be666f
Maxime HEBRARD <maxime.hebrard@gmail.com>
2017-06-26 15:09:27 +0900

    [heatmap]Multiple legends

--- app/common/charts/heatmap.js
+++ app/common/charts/heatmap.js
@@ -32,11 +32,10 @@ export default function Chart(p) {
   p.fontSize = p.fontSize || 14;
   p.width = p.width || 800;
   p.height = p.height || 400;
-  p.margin = p.margin || {top: 30, bottom: 5, left: 5, right: 5};
-  p.legend = p.legend || {top: 100, bottom: 100, left: 100, right: 100};
+  p.margin = p.margin || {top: 30, bottom: 5, left: 5, right: 5, padding: 1};
+  p.legend = p.legend || {top: 100, bottom: 100, left: 100, right: 100, padding: 5};
   // p.color = p.color || d4.schemeSet3;
   p.cornerRadius = p.cornerRadius || 3;
-  p.padding = p.padding || 1;
 
   const color = d4.scaleSequential(d4.interpolateOranges);
   const scale = d4.scaleLinear().range([0.000, 1.000]);
@@ -89,16 +88,16 @@ export default function Chart(p) {
     // group for legend
     svg.append('g')
     .attr('transform', `translate(${p.margin.left + p.legend.left}, ${p.margin.top})`)
-    .classed('legendTop', true)
-    .append('text').attr('html', '<h3>legend Top</h3>');
+    .classed('legendTop', true);
     svg.append('g')
     .attr('transform', `translate(${p.margin.left + p.legend.left}, ${p.height - p.margin.bottom - p.legend.bottom})`)
-    .classed('legendBottom', true)
-    .append('text').attr('html', '<h3>legend Bottom</h3>');
+    .classed('legendBottom', true);
     svg.append('g')
-    .attr('transform', `translate(${p.margin.left}, ${p.height - p.margin.bottom})`)
+    .attr('transform', `translate(${p.margin.left}, ${p.margin.top + p.legend.top})`)
     .classed('legendLeft', true);
-    /* ********************************************* */
+    svg.append('g')
+    .attr('transform', `translate(${p.width - p.margin.right - p.legend.right}, ${p.margin.top + p.legend.top})`)
+    .classed('legendRight', true);
   };
 
   // accessor
@@ -135,44 +134,37 @@ export default function Chart(p) {
       });
     });
     // grid
-    p.gridWidth = (p.width - p.margin.left - p.margin.right) / p.labelX.length;
-    p.gridHeight = (p.height - p.margin.top - p.margin.bottom) / p.labelY.length;
+    p.gridWidth = (p.width - p.margin.left - p.legend.left - p.margin.right - p.legend.right) / p.labelX.length;
+    p.gridHeight = (p.height - p.margin.top - p.legend.top - p.margin.bottom - p.legend.bottom) / p.labelY.length;
     scale.domain([0, p.max]);
 
     // C console.log('heatmap', p.heatmap);
 
     // display data
     const rects = d4.select(`#${p.id}`).select('.rects');
-    const labelsX = d4.select(`#${p.id}`).select('.labelsX');
-    const labelsY = d4.select(`#${p.id}`).select('.labelsY');
+    const top = d4.select(`#${p.id}`).select('.legendTop');
+    const bottom = d4.select(`#${p.id}`).select('.legendBottom');
+    const left = d4.select(`#${p.id}`).select('.legendLeft');
+    const right = d4.select(`#${p.id}`).select('.legendRight');
+    // const labelsY = d4.select(`#${p.id}`).select('.labelsY');
 
     p.labelY.sort().forEach((row, j) => {
       // Display labels
-      const line = (j * p.gridHeight) + (p.gridHeight / 2);
-      labelsY.append('path')
-        .attr('id', `map${row}`)
-        .attr('d', `M${p.padding},${line} L${p.margin.right - p.padding},${line}`)
-        .style('opacity', 0);
-      labelsY.append('text')
-        .attr('text-anchor', 'left')
-        .attr('dy', '0.5ex')
-        .style('pointer-events', 'none')
-        .append('textPath')
-          .attr('xlink:href', `#map${row}`)
-          .text(row);
-
+      const line = j * p.gridHeight;
+      const id = row.replace(' ', '_');
+      addLabel('L', left, line, row, id);
+      addLabel('R', right, line, row, id);
       // Define X index
       const y = p.labelY.indexOf(row);
       p.labelX.sort().forEach((cell, i) => {
         const x = p.labelX.indexOf(cell);
-        // C console.log('x', i * p.gridWidth, 'y', j * p.gridHeight, p.heatmap[x][y]);
         // Create rect
         rects.append('rect')
           .datum(p.heatmap[x][y] || 0)
-          .attr('x', (i * p.gridWidth) + p.padding)
-          .attr('y', (j * p.gridHeight) + p.padding)
-          .attr('width', p.gridWidth - (2 * p.padding))
-          .attr('height', p.gridHeight - (2 * p.padding))
+          .attr('x', (i * p.gridWidth) + p.margin.padding)
+          .attr('y', (j * p.gridHeight) + p.margin.padding)
+          .attr('width', p.gridWidth - (2 * p.margin.padding))
+          .attr('height', p.gridHeight - (2 * p.margin.padding))
           .attr('rx', p.cornerRadius)
           .attr('ry', p.cornerRadius)
           .style('opacity', 1)
@@ -188,20 +180,40 @@ export default function Chart(p) {
 
     p.labelX.sort().forEach((col, i) => {
       // Display labels
-      const line = (i * p.gridWidth) + (p.gridWidth / 2);
-      labelsX.append('path')
-        .attr('id', `map${col}`)
-        .attr('d', `M${line},${p.padding} L${line},${p.margin.bottom - p.padding}`)
-        .style('opacity', 0);
-      labelsX.append('text')
-        .attr('text-anchor', 'left')
-        .attr('dy', '0.5ex')
-        .style('pointer-events', 'none')
-        .append('textPath')
-          .attr('xlink:href', `#map${col}`)
-          .text(col);
+      const line = i * p.gridWidth;
+      const id = col.replace(' ', '_');
+      addLabel('T', top, line, col, id);
+      addLabel('B', bottom, line, col, id);
     });
   };
+
+  function addLabel(mode, group, line, txt, id) {
+    let d = '';
+    if (mode === 'L') {
+      d = `M${p.legend.padding},${line + (p.gridHeight / 2)} L${p.legend.left - p.legend.padding},${line + (p.gridHeight / 2)}`;
+    } else if (mode === 'R') {
+      d = `M${p.legend.padding},${line + (p.gridHeight / 2)} L${p.legend.right - p.legend.padding},${line + (p.gridHeight / 2)}`;
+    } else if (mode === 'T') {
+      d = `M${line + p.legend.padding},${p.legend.top - p.legend.padding} L${line + p.gridWidth - p.legend.padding},${p.legend.padding}`;
+    } else if (mode === 'B') {
+      d = `M${line + p.legend.padding},${p.legend.padding} L${line + p.gridWidth - p.legend.padding},${p.legend.bottom - p.legend.padding}`;
+    } else {
+      console.log('Error addLabel mode');
+    }
+
+    // Common
+    group.append('path')
+      .attr('id', `map${mode}${id}`)
+      .attr('d', d)
+      .style('opacity', 0);
+    group.append('text')
+      .attr('text-anchor', 'left')
+      .attr('dy', '0.5ex')
+      .style('pointer-events', 'none')
+      .append('textPath')
+        .attr('xlink:href', `#map${mode}${id}`)
+        .text(txt);
+  }
 
   function tip(state, d) {
     if (state === 'show') {
