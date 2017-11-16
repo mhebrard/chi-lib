a13ba3b367397eeaceeaa05a0bfa2e581f7e4db9
Maxime HEBRARD <maxime.hebrard@gmail.com>
2017-11-16 14:28:40 +0900

    [bar]Add bar chart

---
+++ app/components/charts/bar/component.js
@@ -0,0 +1,49 @@
+import Chart from 'common/charts/bar';
+
+function controller() {
+  const $ctrl = this;
+
+  // views
+  const paramChart = {
+    div: 'charts',
+    id: 'bar',
+    title: 'Bar chart of "layout" node of flare package',
+    legend: {inner: true, bottom: 150, left: 40, padding: 5},
+    cuttoff: 3000,
+    dispatch
+  };
+  const chart = new Chart(paramChart);
+
+  return Object.assign($ctrl, {
+    editorOptions: {
+      data: $ctrl.dataPackage,
+      onChange: update
+    },
+    $onInit() {
+      draw();
+    }
+  });
+
+  function draw() {
+    chart.init();
+    update();
+  }
+
+  function update() {
+    chart.data($ctrl.dataPackage.resources[0].data);
+    chart.update();
+  }
+
+  // dispatch all action to all views
+  function dispatch(action) {
+    chart.consumer(action);
+  }
+}
+
+export default {
+  controller,
+  templateUrl: 'components/charts/bar/template.html',
+  bindings: {
+    dataPackage: '<package'
+  }
+};
